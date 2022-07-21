d83882b6354b189895384bf06dcf42ddd896cebf
Chris Lisangola <jochri3@gmail.com>
2022-07-21 11:17:15 +0300

    1.Creation de la base de donnees

---
+++ LDD/database.sql
@@ -0,0 +1,10 @@
+-- 1.Creation de la base de donnees
+-- CREATE DATABASE <nom_base_de_donnees>;
+CREATE DATABASE crm_poei_17_juillet;
+
+-- 2.Lister toutes les BD
+SHOW DATABASES;
+
+-- 3.Utiliser une BD
+-- USE <nom_base_de_donnees>
+USE crm_poei_17_juillet
