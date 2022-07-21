639c79a0cc87e78349b56a209ea69cb44328ef05
Chris Lisangola <jochri3@gmail.com>
2022-07-21 12:40:50 +0300

    2.Creation de la table clients

--- LDD/database.sql
+++ LDD/database.sql
@@ -7,4 +7,15 @@ SHOW DATABASES;
 
 -- 3.Utiliser une BD
 -- USE <nom_base_de_donnees>
-USE crm_poei_17_juillet
+USE crm_poei_17_juillet;
+
+-- 3.Creation de la table client
+CREATE TABLE clients(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    telephone VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    entreprise VARCHAR(50) NOT NULL
+);
+
